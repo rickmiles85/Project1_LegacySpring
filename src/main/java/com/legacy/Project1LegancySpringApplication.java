847116bd4ce0975e137eb31031525ba65155a4ee
Rick Miles <rick.miles85@gmail.com>
2024-02-27 10:05:58 +0000

    First Commit

---
+++ src/main/java/com/legacy/Project1LegancySpringApplication.java
@@ -0,0 +1,13 @@
+package com.legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1LegancySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1LegancySpringApplication.class, args);
+	}
+
+}
