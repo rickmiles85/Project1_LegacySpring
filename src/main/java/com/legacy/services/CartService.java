a87f42049d36408bc9e94fb496a5b237af61c0cb
Rick Miles <rick.miles85@gmail.com>
2024-02-29 14:57:02 +0000

    Pre Cart Add

--- src/main/java/com/legacy/services/CartService.java
+++ src/main/java/com/legacy/services/CartService.java
@@ -1,5 +1,6 @@
 package com.legacy.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.legacy.domain.Cart;
 import com.legacy.repo.CartRepo;
+import com.qa.legacy.persistence.domain.CartItem;
 
 @Service
 
@@ -23,6 +25,21 @@ public class CartService {
 		super();
 
 		this.cartRepo = cartRepo;
+	}
+
+	private List<CartItem> cartItems = new ArrayList<>();
+
+	public List<CartItem> checkAvailable() {
+		// List<CartItem> available = new ArrayList<>(); - Stretch Goal to check whether
+		// available
+
+		for (CartItem cartItem : cartItems) {
+			if (cartItem.isAvailable()) {
+				available.add(cartItem);
+			}
+		}
+
+		return available;
 	}
 
 	public ResponseEntity<Cart> createCart(Cart newCart) {
@@ -66,6 +83,14 @@ public class CartService {
 		Cart updated = this.cartRepo.save(existing);
 
 		return ResponseEntity.ok(updated);
+	}
+
+	public int calcBill() {
+		int totalBill = 0;
+		for (CartItem cartItem : cartItems) {
+			totalBill += cartItem.updatedPrice();
+		}
+		return totalBill;
 	}
 
 }
